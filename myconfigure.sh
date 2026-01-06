d1585342bad476ae65a1f781a3c9e598922978df
Ben Martin <monkeyiq@users.sourceforge.net>
2026-01-06 15:41:03 +1000

    .

--- myconfigure.sh
+++ myconfigure.sh
@@ -1,4 +1,5 @@
 echo "This is the monkeyiq config invoker. please don't use it."
+export CXXFLAGS=" -Wtemplate-body -Wdeprecated-declarations "
 /Develop/libferrisstreams/configure \
 --disable-stlport --enable-debug   --enable-hiddensymbols \
 --prefix=/usr/local --libdir=/usr/local/lib64
